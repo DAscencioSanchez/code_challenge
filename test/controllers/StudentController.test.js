c43e7757d905c4b09f63a62d7a38a034fd0023c3
DAscencioSanchez <diaspm77@gmail.com>
2022-05-03 15:16:54 -0500

    Correcciones para que StudentController pase los test de unidad

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -2,26 +2,27 @@ const Reader = require('./../../lib/utils/Reader');
 const StudentServices = require("./../../lib/services/StudentServices");
 const StudentController = require("./../../lib/controllers/StudentController");
 
+
 describe("Tests for StudentController.js", () => {
 
     test("1. A test for getAllStudents()", () => {
         const manualAllStudents = Reader.readJsonFile("visualpartners.json");
-        const getAllStudents = StudentController.getAllStudents();
-        expect(getAllStudents).toStrictEqual(manualAllStudents);
+        const allStudents = StudentController.getAllStudents();
+        expect(allStudents).toStrictEqual(manualAllStudents);
     });
 
-    test("2. A test forgetStudentsEmailsByCertification", () => {
+    test("2. A test for getStudentsByCredits()", () => {
         const students = Reader.readJsonFile("visualpartners.json")
-        const manualStudentsEmails = StudentServices.getStudentsEmails(students, certification);
-        const studentEmails = getStudentsEmailsByCertification(certification);
-        expect(manualStudentsByCredits).toStrictEqual(studentsByCredit);
-    });
-
-    test("3. A test for StudentsEmailsByCertification()", () => {
-        const students = Reader.readJsonFile("visualpartners.json")
-        const manualStudentsByCredits = StudentServices.filterStudentsByCredits(students, credits);
+        const manualStudentsByCredits = StudentServices.filterStudentsByCredits(students, 400);
         const studentsByCredit = StudentController.getStudentsByCredits(400);
         expect(manualStudentsByCredits).toStrictEqual(studentsByCredit);
+    });
+
+    test("3. A test for getStudentsEmailsByCertification(certification)", () => {
+        const students = Reader.readJsonFile("visualpartners.json");
+        const manualStudentsEmails = StudentServices.getStudentsEmails(students, true);
+        const studentEmails = StudentController.getStudentsEmailsByCertification(true)    
+        expect(studentEmails).toStrictEqual(manualStudentsEmails);
     });
         
 });
